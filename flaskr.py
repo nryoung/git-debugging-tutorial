29344c35926f0d946aa29dc9a7375b3609d73ed7
Nic Young <nryoung@gmail.com>
2016-04-04 08:54:19 -0400

    chore: remove all comments and doc strings

--- flaskr.py
+++ flaskr.py
@@ -14,11 +14,8 @@ from flask import (
     flash
 )
 
-
-# create our little application :)
 app = Flask(__name__)
 
-# Load default config and override config from an environment variable
 app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'flaskr.db'),
     DEBUG=True,
@@ -30,14 +27,12 @@ app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 
 def connect_db():
-    """Connects to the specific database."""
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
 
 
 def init_db():
-    """Initializes the database."""
     db = get_db()
     with app.open_resource('schema.sql', mode='r') as f:
         db.cursor().executescript(f.read())
@@ -46,15 +41,11 @@ def init_db():
 
 @app.cli.command('initdb')
 def initdb_command():
-    """Creates the database tables."""
     init_db()
     print('Initialized the database.')
 
 
 def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
     if not hasattr(g, 'sqlite_db'):
         g.sqlite_db = connect_db()
     return g.sqlite_db
@@ -62,7 +53,6 @@ def get_db():
 
 @app.teardown_appcontext
 def close_db(error):
-    """Closes the database again at the end of the request."""
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
