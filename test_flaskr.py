a72f199d59c1b66895dbd205a5c7fd19c147db5e
Nic Young <nryoung@gmail.com>
2016-04-04 01:05:58 -0400

    chore: remove doc string from flaskr tests

--- test_flaskr.py
+++ test_flaskr.py
@@ -1,14 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-    Flaskr Tests
-    ~~~~~~~~~~~~
-
-    Tests the Flaskr application.
-
-    :copyright: (c) 2015 by Armin Ronacher.
-    :license: BSD, see LICENSE for more details.
-"""
-
 import pytest
 
 import os
