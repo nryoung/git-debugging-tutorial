e58f5fc41f6c297b119f34fe590c7300b541aa82
Nic Young <nryoung@gmail.com>
2016-04-04 01:02:56 -0400

    chore: remove doc string from flaskr module

--- flaskr.py
+++ flaskr.py
@@ -1,14 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-    Flaskr
-    ~~~~~~
-
-    A microblog example application written as Flask tutorial with
-    Flask and sqlite3.
-
-    :copyright: (c) 2015 by Armin Ronacher.
-    :license: BSD, see LICENSE for more details.
-"""
 
 import os
 from sqlite3 import dbapi2 as sqlite3
